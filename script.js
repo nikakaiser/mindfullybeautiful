777ebd41252ae88901e1e27cd71c4077f4064c2e
nikakaiser <59620329+nikakaiser@users.noreply.github.com>
2020-02-20 11:48:55 -0800

    added lash gallery

---
+++ script.js
@@ -0,0 +1,51 @@
+$(function(){
+         $("#leafeye").on({
+          mouseenter: function(){
+           $(this).attr('src','images/leafeye-bw.png');
+         },
+         mouseleave: function(){
+           $(this).attr('src','images/leafeye.png');
+         }
+         });
+         $("#mountainhair").on({
+          mouseenter: function(){
+           $(this).attr('src','images/mountainhair-bw.png');
+         },
+         mouseleave: function(){
+           $(this).attr('src','images/mountainhair.png');
+         }
+         });
+});
+
+'use strict'
+
+
+function toggleIcon() {
+    $('.icon').on('click', function(){
+        $('.icon').toggleClass('active');
+        $('.menu-mobile').slideToggle(300);
+    })
+}
+
+function closeMobileMenu(){
+    $('.menu-mobile').on('click', 'a', function(){
+        $('.icon').trigger('click');
+    })
+}
+
+
+
+function desktopMenu() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50 && $(window).width() > 768) {
+            $('header.desktop').fadeIn(500);
+        } else {
+            $('header.desktop').fadeOut(500);
+        }
+    })
+}
+
+//when the page loads call toggleIcon;
+$(toggleIcon);
+$(closeMobileMenu);
+$(desktopMenu);
